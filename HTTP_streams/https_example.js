8b6daf5d9fd14d23cfa2eec067a6ef9e89988240
MikeEngerer <michael.engerer@hotmail.com>
2018-12-03 01:11:37 +0000

    added res in callback

--- HTTP_streams/https_example.js
+++ HTTP_streams/https_example.js
@@ -6,8 +6,9 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(res) {
   console.log('In response handler callback!');
+  console.log("Response: ", res)
 }
 
 console.log("I'm about to make the request!");
@@ -15,3 +16,15 @@ console.log("I'm about to make the request!");
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
+/*vagrant [HTTP_streams]> node https_example.js 
+I'm about to make the request!
+I've made the request!
+events.js:183
+      throw er; // Unhandled 'error' event
+      ^
+
+Error: getaddrinfo ENOTFOUND example.org example.org:443
+    at errnoException (dns.js:50:10)
+    at GetAddrInfoReqWrap.onlookup [as oncomplete] (dns.js:92:26)
+    */
